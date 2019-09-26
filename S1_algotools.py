c043c95fcfdba8d542343adc144ba5bae6892003
Thiebault BEBERT <bebertt@iut-acy.local>
2019-09-26 17:04:51 +0200

    mise à jour exo 2

--- S1_algotools.py
+++ S1_algotools.py
@@ -9,19 +9,32 @@ Created on Thu Sep 26 14:17:05 2019
 # d'indice de celui-ci, avant de diviser le nombre max optenu par le nombre
 # total d'indice obtenus.
 
-def average_above_zero(table:list):
+def average_above_zero(tab):
+    if not(isinstance(tab, list)):
+        raise ValueError('average_above_zero, expected a list as input')
     somme = 0
     n = 0
-    for i in range(len(table)):
-        if table[i] > 0 :
+    for i in range(len(tab)):
+        if tab[i] > 0 :
             somme = somme + tab[i]
             n = n + 1
     if n == 0:
         return print ('Division par 0 impossible')
     moyenne = (somme / n)
     return moyenne
-## Test de la fonction
+# Test de la fonction
 tab = [1,2,3,4]
 somme = average_above_zero(tab)
 print('resultat =', somme)
 
+# Fonction qui récupère le nombre max d'une liste.
+def max_value(tab):
+    for i in range(len(tab)):
+        if i==max(tab):
+            return tab[i]
+        else:
+         return max(tab)
+# Test de la fonction
+tab = [1,4,3]
+res = max_value(tab)
+print('Resultat =', res)
