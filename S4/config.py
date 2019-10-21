67584464a7c0f21e14a15a32e185e34c85a20108
Thiebault BEBERT <bebertt@iut-acy.local>
2019-10-21 08:49:59 +0200

    Mise à jour

---
+++ S4/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 08:43:00 2019
+
+@author: bebertt
+"""
+
+amqp_url = 'amqp://xyditfwg:NZFLxcQr4S73XM3lgpnebte42Rf_n0kl@dove.rmq.cloudamqp.com/xyditfwg';
+userName = 'xyditfwg';
